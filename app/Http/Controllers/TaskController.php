e938b4147d702e46697a8ec83d27f2d9eb56edd3
Laurin49 <welter@diwe.info>
2024-11-18 18:12:12 +0100

    dev01_crud_operations o.k.

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return new TaskCollection(Task::all());
+  }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreTaskRequest $request)
+  {
+    $validated = $request->validated();
+    $task = Task::create($validated);
+    
+    return new TaskResource($task);
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(Task $task)
+  {
+    return TaskResource::make($task);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateTaskRequest $request, Task $task)
+  {
+    $validated = $request->validated();
+    $task->update($validated);
+
+    return new TaskResource($task);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Task $task)
+  {
+    $task->delete();
+    return response()->noContent();
+  }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    User::create([
+      'name' => 'Admin',
+      'email' => 'admin@hsv.de',
+      'password' => bcrypt('hsv1887tv'),
+      'email_verified_at' => now(),
+    ]);
+    User::create([
+      'name' => 'Welter',
+      'email' => 'welter@hsv.de',
+      'password' => bcrypt('hsv1887tv'),
+      'email_verified_at' => now(),
+    ]);
+    User::create([
+      'name' => 'Schonlau',
+      'email' => 'schonlau@hsv.de',
+      'password' => bcrypt('hsv1887tv'),
+      'email_verified_at' => now(),
+    ]);
+  }
+}
