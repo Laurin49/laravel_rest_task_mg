668d7dd2d35514e5379635504f833ba5b49c32cf
Laurin49 <welter@diwe.info>
2024-11-21 15:27:09 +0100

    dev06_nested_resources

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MembersController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
@@ -11,10 +12,15 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 })->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
   Route::apiResource('tasks', TaskController::class);
   Route::apiResource('projects', ProjectController::class);
+  Route::apiResource('projects.members', MembersController::class)->only([
+    'index',
+    'store',
+    'destroy',
+  ]);
 });
